2e7077b575195ec79d03f4a527f38fe0f337fdc3
samanthakendig <kendigs@oregonstate.edu>
2020-02-10 19:09:59 -0800

    it works as an echo machine

--- smallsh.c
+++ smallsh.c
@@ -6,9 +6,26 @@
 #include <fcntl.h>
 #include <time.h>
 
+#define MAX_ARGS 512
+#define ARG_MAX_LENGTH 2048
+
 int main (int* argc, char** argv) {
-    printf(": ");
-    fflush(stdout);
+    int n_chars = -5, i = 0;
+    size_t buf_size = -5;
+    char* line = NULL;
+    char args[MAX_ARGS][ARG_MAX_LENGTH];
+
+    for (i = 0; i < MAX_ARGS; i++) {
+        memset(args[i], '\0', sizeof(args[i]));
+    }
+    
+    while (1) {
+        printf(": ");
+        fflush(stdout);
+        n_chars = getline(&line, &buf_size, stdin);
+        printf("%s", line);
+        fflush(stdout);
+    }
 
     return 0;
 }
