e89360d455bf6a50bd461c845221dd8667ff4a0f
samanthakendig <kendigs@oregonstate.edu>
2020-02-10 18:42:52 -0800

    adding basic test code

--- smallsh.c
+++ smallsh.c
@@ -1,1 +1,14 @@
-//oh yeah babey
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+
+int main (int* argc, char** argv) {
+    printf(": ");
+    fflush(stdout);
+
+    return 0;
+}
