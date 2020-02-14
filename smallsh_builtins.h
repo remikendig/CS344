f6cd684314726e0487e8da60a359c831d9d120a9
samanthakendig <kendigs@oregonstate.edu>
2020-02-13 22:24:26 -0800

    updated smallsh_status function prototype

--- smallsh_builtins.h
+++ smallsh_builtins.h
@@ -1,8 +1,17 @@
 #ifndef __SMALLSH_BUILTINS_H
 #define __SMALLSH_BUILTINS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <unistd.h>
+
 void smallsh_exit();
 void smallsh_cd(char* path);
-void smallsh_status();
+void smallsh_status(int p_status, bool if_sig);
 
 #endif
