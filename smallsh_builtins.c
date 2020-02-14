40daa805e9baf5119eaf83a0283883cda6f0f2cb
samanthakendig <kendigs@oregonstate.edu>
2020-02-13 22:07:07 -0800

    made prelim smallsh_cd

--- smallsh_builtins.c
+++ smallsh_builtins.c
@@ -1,5 +1,22 @@
 #include "smallsh_builtins.h"
+#include <unistd.h>
 
 void smallsh_exit() {
     
+}
+
+void smallsh_cd(char* path) {
+    char homeenv[1000];
+
+    strcpy(homeenv, getenv("HOME"));
+
+    if (path == NULL) {
+        chdir(homeenv);
+    } else {
+        chdir(path);
+    }
+}
+
+void smallsh_status() {
+
 }
