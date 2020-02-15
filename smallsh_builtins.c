9ab26ee811294320bd6852bd5f3f58d5afa8e892
samanthakendig <kendigs@oregonstate.edu>
2020-02-14 20:56:45 -0800

    added comment blocks to all functions

--- smallsh_builtins.c
+++ smallsh_builtins.c
@@ -1,9 +1,26 @@
 #include "smallsh_builtins.h"
 
+/*
+*   Function: smallsh_exit()
+*   Status: INCOMPLETE
+*   Purpose: To kill all child processes, foreground and background,
+*            and exit the shell.
+*   Preconditions: N/A
+*   Postconditions: The shell is exited.
+*/
 void smallsh_exit(struct dynarray* processes) {
     
 }
 
+/*
+*   Function: smallsh_cd()
+*   Status: DONE
+*   Purpose: To function the same as cd in bash; changes to the directory
+*            specified by path, or the path specified by the HOME environment
+*            variable if no path is specified.
+*   Preconditions: path is a valid string, or NULL.
+*   Postconditions: The working directory of the program is changed.
+*/
 void smallsh_cd(char* path) {
     char homeenv[1000];
 
@@ -16,6 +33,16 @@ void smallsh_cd(char* path) {
     }
 }
 
+/*
+*   Function: smallsh_status()
+*   Status: UNTESTED
+*   Purpose: To print out the exit status of the last foreground
+*            process to run. If no foreground processes have been
+*            run, prints out 0. Also specifies whether the process
+*            ended or was terminated by a signal.
+*   Preconditions: N/A
+*   Postconditions: N/A
+*/
 void smallsh_status(int p_status, bool if_sig) {
     if (if_sig) {
         printf("terminated by signal %d", p_status);
