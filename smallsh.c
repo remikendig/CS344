a715600ee45d1ff69e5fcba49ad0dffbeaf7ad7b
samanthakendig <kendigs@oregonstate.edu>
2020-02-15 20:11:58 -0800

    fixed a small error w/ my dynarray

--- smallsh.c
+++ smallsh.c
@@ -28,7 +28,7 @@ int main (int* argc, char** argv) {
             continue;
         }
 
-        dynarray_set(args, 0, token);
+        dynarray_insert(args, token);
 
         if (line[0] == '#') {
             ;
